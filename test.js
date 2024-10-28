8b8a0730b4d9590534880b7d996bd405c8aca34a
traponsix <111047578+traponsix@users.noreply.github.com>
2024-10-28 18:40:43 +0100

    corrected function

--- test.js
+++ test.js
@@ -17,8 +17,8 @@ button.addEventListener('click', function() {
     }
 
     //find x
-    var t = Math.exp(0.77)
-    var b = (Math.log2(a / 1.5) / Math.log2(t)) *2
+    var t = Math.exp(0.385)
+    var b = (Math.log2(a / 1.5) / Math.log2(t))
 
     //round the function to the lowest int
     var c = Math.floor(b);
