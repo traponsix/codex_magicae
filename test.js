55cb7f6897bdd24511075e0b102716d20bced311
traponsix <111047578+traponsix@users.noreply.github.com>
2024-10-28 17:56:50 +0100

    Fixed the curve value calculator
    also added multiple measurment sysyem support

--- test.js
+++ test.js
@@ -1,11 +1,26 @@
 let d = new Date();
-var a = 1.5;
 
-//find x
-//I don't understand where I should multiply 1.5
-var t = Math.exp(0.77)
-var b = (Math.log2(a / 1.5) / Math.log2(t)) *2
+const button = document.getElementById('addGadgetUrl');
+button.addEventListener('click', function() {
+    var a = document.getElementById("height").value;
+    if(a == null) {
+        a = 1.8;
+    }
 
-//round the function to the lowest int
-var c = Math.floor(b);
-document.getElementById("curve").innerHTML = b + " --> " + c;
+    var unit = document.getElementById('unit').value;
+    
+    if(unit=='meters'){
+
+    }
+    else if(unit=='feets'){
+        a = a / 3.28084;
+    }
+
+    //find x
+    var t = Math.exp(0.77)
+    var b = (Math.log2(a / 1.5) / Math.log2(t)) *2
+
+    //round the function to the lowest int
+    var c = Math.floor(b);
+    document.getElementById("curve").innerHTML = b + " --> " + c;
+});
